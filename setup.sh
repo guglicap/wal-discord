9e9fc3abdc286dcb524205d4bc09b454384053bc
goo <guglielmocaprotti@gmail.com>
2018-05-15 22:42:00 +0200

    modified master for default wal backend

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir -p $HOME/.config/wal-discord
-cp master.scss $HOME/.config/wal-discord
+cp *.scss $HOME/.config/wal-discord
+ln -sf $HOME/.config/wal-discord/master-wal.scss $HOME/.config/wal-discord/master.scss
